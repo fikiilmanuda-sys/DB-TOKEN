b522320826c58abd4e91eee2feb912cb95f70874
fikiilmanuda-sys <fikiilmanuda@gmail.com>
2026-06-18 23:18:24 +0700

    Update tokens

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8955856809:AAEmWpSNjOHwJelPgivABNU5KmIF6uqKxmU"
+    "8955856809:AAEmWpSNjOHwJelPgivABNU5KmIF6uqKxmU",
+    "8813692038:AAHcBEPk0iEackn0-BtUiSSoHx2UQrlYJ0M"
   ]
 }
