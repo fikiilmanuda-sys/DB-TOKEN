3364e7524a691f7b5eb30185df7d7d31f7afd940
fikiilmanuda-sys <fikiilmanuda@gmail.com>
2026-06-19 19:12:43 +0700

    Update tokens

--- token.js
+++ token.js
@@ -2,6 +2,7 @@
   "tokens": [
     "8955856809:AAEmWpSNjOHwJelPgivABNU5KmIF6uqKxmU",
     "8813692038:AAHcBEPk0iEackn0-BtUiSSoHx2UQrlYJ0M",
-    "8819838252:AAGicjcfzbtkc01wjuqxLIgIDmdvBusSYyY"
+    "8819838252:AAGicjcfzbtkc01wjuqxLIgIDmdvBusSYyY",
+    "8712107509:AAHHYP1iwehzgg7ACG3KHzdZe-1_rWvVTJA"
   ]
 }
